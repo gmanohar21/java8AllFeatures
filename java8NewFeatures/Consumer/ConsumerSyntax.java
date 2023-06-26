e38718ca92304694e4bf580694fb16fd80084429
gmanohar21 <gmanohar21@gmail.com>
2023-06-26 20:49:37 +0530

    consumer modification

---
+++ java8NewFeatures/Consumer/ConsumerSyntax.java
@@ -0,0 +1,23 @@
+package java8NewFeatures.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerSyntax {
+    public static void main(String[] args) {
+        // A consumer function in Java is a functional
+        // interface that represents an operation that
+        // takes in a single input argument and performs
+        // some action without returning any result.
+
+        // @FunctionalInterface
+        // public interface Consumer<T> {
+        // void accept(T t);
+        // }
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "Dave");
+
+        Consumer<String> consume=n->names.forEach(System.out.println(n););
+    }
+}
