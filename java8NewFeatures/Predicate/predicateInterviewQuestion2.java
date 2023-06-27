a4b548698794ba2bb5c30e388fd6758888f2501b
gmanohar21 <gmanohar21@gmail.com>
2023-06-27 08:45:02 +0530

    predicate modify

--- java8NewFeatures/Predicate/predicateInterviewQuestion2.java
+++ java8NewFeatures/Predicate/predicateInterviewQuestion2.java
@@ -6,17 +6,17 @@ import java.util.function.Predicate;
 
 public class predicateInterviewQuestion2 {
     public static void main(String[] args) {
-        List<String> fruit=List.of("Apple", "mango", "Banana", "Guava", "grapes");
-        Predicate<String> fruitPredicate=n->n.toUpperCase().startsWith("M");
-        List<String> filteredList=filter(fruit,fruitPredicate);
+        List<String> fruit = List.of("Apple", "mango", "Banana", "Guava", "grapes");
+        Predicate<String> fruitPredicate = n -> n.toUpperCase().startsWith("M");
+        List<String> filteredList = filter(fruit, fruitPredicate);
         System.out.println(filteredList.toString());
     }
 
-    public static List<String> filter(List<String> frt,Predicate<String> p){
-        List<String> filteredList=new ArrayList<>();
-        for(String frNAme:frt){
-            if(p.test(frNAme)){
-                      filteredList.add(frNAme);
+    public static List<String> filter(List<String> frt, Predicate<String> p) {
+        List<String> filteredList = new ArrayList<>();
+        for (String frNAme : frt) {
+            if (p.test(frNAme)) {
+                filteredList.add(frNAme);
             }
         }
         return filteredList;
